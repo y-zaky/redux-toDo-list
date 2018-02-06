b1f79f3d672fcd85d61aec532939cd18c48e28fc
Yahia <zaky20@hotmail.com>
2018-02-06 18:04:24 +0000

    toDoList rendered with class

--- components/List.js
+++ components/List.js
@@ -1,34 +1,26 @@
 import React, { Component } from 'react'
 
-// export default class List extends Component {
-//   render() {
-//     const renderList = (this.props.value) => {
+export default class List extends Component {
+  render () {
 
-//       const listItems = this.props.value
-//       console.log('listItems', listItems)
-//       return listItems.map (
-//         (toDo, index) => {
-//           console.log('toDooo', toDo)
-//           return <li key={index}>{toDo}</li>  
-//       })
+    const toDosListArr = this.props.value
+    const toDos = toDosListArr.map( (toDo, index) => <li key={index}>{toDo}</li> )
 
-//     }
+    return (
+      <div> {toDos} </div>
+    )
 
-//     return (
-//       <div> {renderList} </div>
-//     )
+  }
 
-//   }
-
-// }
-
-export default function List (props) {
-  const toDos = props.value 
-  console.log(toDos)
-  const list = toDos.map(
-    (toDo, index) => <li key={index}>{toDo}</li>
-  )
-  return (
-    <h1>{list}</h1>
-  )
 }
+
+// export default function List (props) {
+//   const toDos = props.value 
+//   console.log(toDos)
+//   const list = toDos.map(
+//     (toDo, index) => <li key={index}>{toDo}</li>
+//   )
+//   return (
+//     <h1>{list}</h1>
+//   )
+// }
