6c684656d47ccf6a03e4d125c25f7f6c9c60aeda
Yahia <zaky20@hotmail.com>
2018-02-08 09:09:27 +0000

    added input for editToDo

--- components/List.js
+++ components/List.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 export default class List extends Component {
+  constructor(props) {
+    super(props)
+
+    this.editToDo = this.editToDo.bind(this)
+  }
+
+  editToDo (toDoObj) {
+
+     const main =  document.getElementById(`hi${toDoObj.index}`);
+     const input = document.createElement('input');
+     main.appendChild(input);
+  }
+
   render () {
 
     const toDosListArr = this.props.value
@@ -8,9 +21,10 @@ export default class List extends Component {
       (toDo, index) => {
     
         return (
-          <div key={index}>
-            <li >{toDo}</li>
-            <button onClick={ () => {this.props.delete({toDo, index})} }>Delete</button>
+          <div id={`hi${index}`} key={index}>
+            <li >{index}.  {toDo}</li>
+            <button onClick={ () => {console.log('this', this); this.props.delete({toDo, index})} }>Delete</button>
+            <button onClick={ () => this.editToDo({toDo, index}) }>Edit</button>
           </div>
         )
       }
