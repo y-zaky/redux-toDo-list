811a272e37d6813fb8baf832184f649100b7f640
Yahia <zaky20@hotmail.com>
2018-02-15 10:13:48 +0000

    fixed console error by composing middleware

--- store.js
+++ store.js
@@ -1,8 +1,8 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import rootReducer from './reducers'
 
 const middleware = applyMiddleware(thunk)
 
-export default createStore(rootReducer, middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default createStore(rootReducer, compose(middleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
